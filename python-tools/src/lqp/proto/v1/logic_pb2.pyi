b08dacc4a5303d6a5350deac11ddeb0848771265
David Zhao <david.zhao@relational.ai>
2025-06-25 23:16:26 +1000

    Update proto to add Int64 and Int128

--- python-tools/src/lqp/proto/v1/logic_pb2.pyi
+++ python-tools/src/lqp/proto/v1/logic_pb2.pyi
@@ -13,6 +13,8 @@ class PrimitiveType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     PRIMITIVE_TYPE_INT: _ClassVar[PrimitiveType]
     PRIMITIVE_TYPE_FLOAT: _ClassVar[PrimitiveType]
     PRIMITIVE_TYPE_UINT128: _ClassVar[PrimitiveType]
+    PRIMITIVE_TYPE_INT64: _ClassVar[PrimitiveType]
+    PRIMITIVE_TYPE_INT128: _ClassVar[PrimitiveType]
 
 class RelValueType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = ()
@@ -37,6 +39,8 @@ PRIMITIVE_TYPE_STRING: PrimitiveType
 PRIMITIVE_TYPE_INT: PrimitiveType
 PRIMITIVE_TYPE_FLOAT: PrimitiveType
 PRIMITIVE_TYPE_UINT128: PrimitiveType
+PRIMITIVE_TYPE_INT64: PrimitiveType
+PRIMITIVE_TYPE_INT128: PrimitiveType
 REL_VALUE_TYPE_UNSPECIFIED: RelValueType
 REL_VALUE_TYPE_DECIMAL: RelValueType
 REL_VALUE_TYPE_DATE: RelValueType
