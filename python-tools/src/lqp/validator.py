f0cedfbd0e49fe96b453179af667006527e803f1
Mohamad Barbar <mbarbar@users.noreply.github.com>
2025-06-26 14:19:02 +1000

    Don't limit duplication ID error to within fragment (#47)

--- python-tools/src/lqp/validator.py
+++ python-tools/src/lqp/validator.py
@@ -82,20 +82,25 @@ class ShadowedVariableFinder(LqpVisitor):
 
         self.visit(node.value, in_scope_names | set(v[0].name for v in node.vars))
 
-# Checks for duplicate RelationIds within a fragment.
+# Checks for duplicate RelationIds.
 # Raises ValidationError upon encountering such.
 class DuplicateRelationIdFinder(LqpVisitor):
-    def visit_Fragment(self, node: ir.Fragment, *args: Any) -> None:
-        seen_ids = set()
-        for decl in node.declarations:
-            # Of Declarations, only Defs have relation IDs.
-            if isinstance(decl, ir.Def):
-                if decl.name in seen_ids:
-                    raise ValidationError(
-                        f"Duplicate declaration within fragment at {decl.meta}: '{decl.name.id}'"
-                    )
-                else:
-                    seen_ids.add(decl.name)
+    def __init__(self):
+        self.seen_ids: ir.RelationId = set()
+
+    def visit_Def(self, node: ir.Def, *args: Any) -> None:
+        if node.name in self.seen_ids:
+            raise ValidationError(
+                f"Duplicate declaration at {node.meta}: '{node.name.id}'"
+            )
+        else:
+            self.seen_ids.add(node.name)
+
+    def visit_Loop(self, node: ir.Loop, *args: Any) -> None:
+        # Only the Defs in init are globally visible so don't visit body Defs.
+        # TODO: add test for non-/duplicates associated with loops.
+        for d in node.init:
+            self.visit(d)
 
 
 def validate_lqp(lqp: ir.LqpNode):
