caaf64c0549498ab694fbd8b87df1a5592bbcffc
David Zhao <david.zhao@relational.ai>
2025-08-19 11:29:29 +1000

    Fix decimal printing

--- python-tools/src/lqp/print.py
+++ python-tools/src/lqp/print.py
@@ -386,14 +386,11 @@ def to_str(node: Union[ir.LqpNode, ir.Type, ir.Value, ir.SpecializedValue, int, 
     elif isinstance(node, ir.DecimalValue):
         _, _, exponent = node.value.as_tuple()
         assert isinstance(exponent, int)
-        decimal_val = node.value / (10 ** (node.scale + exponent))
-        # Format the decimal to prevent scientific notation
-        lqp += f"{ind}{'{:,f}'.format(decimal_val)}d{node.precision}"
+        # Format the decimal to have the correct scale
+        lqp += f"{ind}{node.value:.{node.scale}f}d{node.precision}"
     elif isinstance(node, ir.DateValue):
-        # lqp += f"{ind}{node.value.isoformat()}"
         lqp += f"{ind}{conf.LPAREN()}{conf.kw('date')} {node.value.year} {node.value.month} {node.value.day}{conf.RPAREN()}"
     elif isinstance(node, ir.DateTimeValue):
-        # lqp += f"{ind}{node.value.isoformat()}"
         lqp += f"{ind}{conf.LPAREN()}{conf.kw('datetime')} {node.value.year} {node.value.month} {node.value.day} {node.value.hour} {node.value.minute} {node.value.second} {node.value.microsecond}{conf.RPAREN()}"
 
     elif isinstance(node, (int, float)):
