8e8715fa5a4bf8f3961ebecbecfcefb04d3e4268
Nate Nystrom <nate.nystrom@gmail.com>
2026-02-20 13:29:25 +0100

    refactor tests

--- sdks/julia/LogicalQueryProtocol/test/parser_tests.jl
+++ sdks/julia/LogicalQueryProtocol/test/parser_tests.jl
@@ -1,5 +1,4 @@
-@testitem "Parser - basic" begin
-    using LogicalQueryProtocol: parse, Proto
+@testitem "Parser - basic" setup=[ParserSetup] begin
 
     input = """
     (transaction
@@ -7,47 +6,40 @@
         (writes)
         (reads)))
     """
-    result = parse(input)
+    result = Parser.parse(input)
     @test !isnothing(result)
     @test result isa Proto.Transaction
     @test length(result.epochs) == 1
 end
 
-@testitem "Parser - error on empty input" begin
-    using LogicalQueryProtocol: parse, ParseError
-    @test_throws ParseError parse("")
-    @test_throws ParseError parse("   ")
-    @test_throws ParseError parse("\n\n")
+@testitem "Parser - error on empty input" setup=[ParserSetup] begin
+    @test_throws ParseError Parser.parse("")
+    @test_throws ParseError Parser.parse("   ")
+    @test_throws ParseError Parser.parse("\n\n")
 end
 
-@testitem "Parser - error on unclosed paren" begin
-    using LogicalQueryProtocol: parse, ParseError
-    @test_throws ParseError parse("(transaction")
-    @test_throws ParseError parse("(transaction (epoch (writes) (reads))")
+@testitem "Parser - error on unclosed paren" setup=[ParserSetup] begin
+    @test_throws ParseError Parser.parse("(transaction")
+    @test_throws ParseError Parser.parse("(transaction (epoch (writes) (reads))")
 end
 
-@testitem "Parser - error on missing transaction wrapper" begin
-    using LogicalQueryProtocol: parse, ParseError
-    @test_throws ParseError parse("(epoch (writes) (reads))")
+@testitem "Parser - error on missing transaction wrapper" setup=[ParserSetup] begin
+    @test_throws ParseError Parser.parse("(epoch (writes) (reads))")
 end
 
-@testitem "Parser - error on bad token" begin
-    using LogicalQueryProtocol: parse, ParseError
-    @test_throws ParseError parse("(transaction @@@)")
+@testitem "Parser - error on bad token" setup=[ParserSetup] begin
+    @test_throws ParseError Parser.parse("(transaction @@@)")
 end
 
-@testitem "Parser - error on trailing garbage" begin
-    using LogicalQueryProtocol: parse, ParseError
-    @test_throws ParseError parse("(transaction (epoch (writes) (reads))) extra")
+@testitem "Parser - error on trailing garbage" setup=[ParserSetup] begin
+    @test_throws ParseError Parser.parse("(transaction (epoch (writes) (reads))) extra")
 end
 
-@testitem "Parser - error on unterminated string" begin
-    using LogicalQueryProtocol: parse, ParseError
-    @test_throws ParseError parse("(transaction \"hello")
+@testitem "Parser - error on unterminated string" setup=[ParserSetup] begin
+    @test_throws ParseError Parser.parse("(transaction \"hello")
 end
 
-@testitem "Parser - scan_string" begin
-    using LogicalQueryProtocol: scan_string
+@testitem "Parser - scan_string" setup=[ParserSetup] begin
     @test scan_string("\"hello\"") == "hello"
     @test scan_string("\"\"") == ""
     @test scan_string("\"a\\nb\"") == "a\nb"
@@ -59,8 +51,7 @@ end
     @test scan_string("\"a\\\\nb\"") == "a\\nb"
 end
 
-@testitem "Parser - scan_int" begin
-    using LogicalQueryProtocol: scan_int
+@testitem "Parser - scan_int" setup=[ParserSetup] begin
     @test scan_int("0") == 0
     @test scan_int("42") == 42
     @test scan_int("-1") == -1
@@ -68,8 +59,7 @@ end
     @test scan_int(string(typemin(Int64))) == typemin(Int64)
 end
 
-@testitem "Parser - scan_float" begin
-    using LogicalQueryProtocol: scan_float
+@testitem "Parser - scan_float" setup=[ParserSetup] begin
     @test scan_float("3.14") == 3.14
     @test scan_float("-1.5") == -1.5
     @test scan_float("inf") == Inf
@@ -77,8 +67,7 @@ end
     @test isnan(scan_float("nan"))
 end
 
-@testitem "Parser - scan_int128" begin
-    using LogicalQueryProtocol: scan_int128, Proto
+@testitem "Parser - scan_int128" setup=[ParserSetup] begin
     r = scan_int128("0i128")
     @test r isa Proto.Int128Value
     @test r.low == 0
@@ -93,8 +82,7 @@ end
     @test r.high == typemax(UInt64)
 end
 
-@testitem "Parser - scan_uint128" begin
-    using LogicalQueryProtocol: scan_uint128, Proto
+@testitem "Parser - scan_uint128" setup=[ParserSetup] begin
     r = scan_uint128("0x0")
     @test r isa Proto.UInt128Value
     @test r.low == 0
@@ -109,8 +97,7 @@ end
     @test r.high == 1
 end
 
-@testitem "Parser - scan_decimal" begin
-    using LogicalQueryProtocol: scan_decimal, Proto
+@testitem "Parser - scan_decimal" setup=[ParserSetup] begin
     r = scan_decimal("123.456d6")
     @test r isa Proto.DecimalValue
     @test r.precision == 6
@@ -125,8 +112,7 @@ end
     @test r.scale == 1
 end
 
-@testitem "Parser - Lexer tokenization" begin
-    using LogicalQueryProtocol: Lexer
+@testitem "Parser - Lexer tokenization" setup=[ParserSetup] begin
     lexer = Lexer("(transaction (epoch (writes) (reads)))")
     # Tokens: ( transaction ( epoch ( writes ) ( reads ) ) ) $
     @test length(lexer.tokens) == 13  # 12 meaningful + EOF
@@ -137,8 +123,7 @@ end
     @test lexer.tokens[end].type == "\$"
 end
 
-@testitem "Parser - round trip" begin
-    using LogicalQueryProtocol: parse, Proto
+@testitem "Parser - round trip" setup=[ParserSetup] begin
     using ProtoBuf: ProtoBuf
 
     test_files_dir = joinpath(@__DIR__, "lqp")
@@ -153,7 +138,7 @@ end
         bin_path = joinpath(bin_files_dir, replace(lqp_file, ".lqp" => ".bin"))
 
         content = read(lqp_path, String)
-        result = parse(content)
+        result = Parser.parse(content)
         @test !isnothing(result)
         @test result isa Proto.Transaction
 
